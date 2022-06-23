d66649fa2042bd7a2cc6aad50a3b11e428b28c14
RileyPorter <por19006@byui.edu>
2022-06-23 13:48:59 -0600

    June twenty third update

--- Login.js
+++ Login.js
@@ -1,5 +1,35 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Text, TouchableOpacity } from "react-native";
+
+const sendText = async (phoneNumber) => {
+  console.log("PhoneNumber: ",phoneNumber);
+  const loginResponse=await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber,{
+    method: 'POST',
+    headers:{
+        'content-type':'application/text'
+     } 
+    });
+const loginResponseText = await loginResponse.text(); //converts the promise to a string by using await
+console.log('Login Response', loginResponseText);
+
+  };
+
+const getToken = async({phoneNumber,oneTimePassword}) =>{//THIS CODE IS NOT COMPLETE
+  const loginResponse=await fetch ('https://dev.stedi.me/twofactorlogin',{
+    method: 'POST',
+    headers:{
+      'content-type':'application/text'
+    },
+    body:{
+      phoneNumber,
+      oneTimePassword
+    }
+  });
+  const token = await loginResponse.text();
+  console.log(token);
+}
+
+
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -13,6 +43,15 @@ const Login = () => {
         value={phoneNumber}
         placeholder="801-555-1212"
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{
+          sendText(phoneNumber)
+          console.log('Login button was clicked')}}
+      >
+        <Text>Send Text</Text>
+      </TouchableOpacity>
+
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
@@ -21,6 +60,12 @@ const Login = () => {
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{console.log('Login button was clicked')}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -32,6 +77,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  mainView:{
+    marginTip:100
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  
+  }
 });
 
 export default Login;
